ad1d7c6d3887e13ed9c1f8303a393cffa54a07aa
weightedEights <poormanjosh@gmail.com>
2019-09-23 16:37:10 -0700

    case_study_3 work; add scipy

--- Wk3/case_study_3.py
+++ Wk3/case_study_3.py
@@ -5,10 +5,44 @@ Docstring
 """
 
 import numpy as np
+import random
+import scipy.stats as ss
 
 
 def main():
-    print("Hello, World!")
+
+    p1 = np.array([1, 1])
+    p2 = np.array([4, 4])
+
+    votes = [1, 2, 3, 1, 2, 3, 1, 2, 3, 3, 3, 3, 3]
+
+    print(majority_vote(votes))
+
+
+def distance(p1, p2):
+    """Calculate distance between p1 and p2"""
+    return np.sqrt(np.sum(np.power(p2 - p1, 2)))
+
+
+def majority_vote(votes):
+    """
+    xxx
+    """
+    vote_counts = {}
+    for v in votes:
+        if v in vote_counts:
+            vote_counts[v] += 1
+        else:
+            vote_counts[v] = 1
+
+    winners = []
+    max_count = max(vote_counts.values())
+    for vote, count in vote_counts.items():
+        if count == max_count:
+            winners.append(vote)
+
+    # if tie, choose any
+    return random.choice(winners)
 
 
 if __name__ == '__main__':
